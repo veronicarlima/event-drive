0a4281b70bb2eaae436bb0f15db933617f1d20c1
veronica.ramires <veronica_ramires@live.com>
2026-04-25 11:14:53 -0400

    Initial commit

---
+++ src/main/java/com/vramires/kafka/event_drive/EventDriveApplication.java
@@ -0,0 +1,13 @@
+package com.vramires.kafka.event_drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vramires/kafka/event_drive/EventDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vramires.kafka.event_drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
